abc7632c27d87d87a57d91e33d0b20d1adc745bc
CurtisWhall <49003469+CurtisWhall@users.noreply.github.com>
2021-04-04 03:04:49 -0400

    Updated insert.php

--- insert.php
+++ insert.php
@@ -9,30 +9,25 @@ try {
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-}
-   $stmt = $conn->prepare("INSERT INTO Users (firstname, lastname, 
-email, password) 
-VALUES (:firstname, :lastname, :email, :password)");
+  $stmt = $conn->prepare("INSERT INTO Users (firstname, lastname, 
+email, upassword) 
+VALUES (:firstname, :lastname, :email, :upassword)");
     $stmt->bindParam(':firstname', $firstname);
     $stmt->bindParam(':lastname', $lastname);
     $stmt->bindParam(':email', $email);
-    $stmt->bindParam(':password', $password)
+    $stmt->bindParam(':upassword', $upassword)
 
 // insert a row
     $firstname = $_POST["firstname"];
     $lastname = $_POST["lastname"];
     $email = $_POST["email"];
-    $password = $_POST["password"];
+    $upassword = $_POST["upassword"];
     $stmt->execute();
 
 
     echo "New records created successfully";
-}
-catch(PDOException $e)
-{
-    echo "Error: " . $e->getMessage();
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
 $conn = null;
 ?>
