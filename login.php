e0c7967e07de77f7bdaaa3fe8523b275d8a97908
CurtisWhall <49003469+CurtisWhall@users.noreply.github.com>
2021-04-05 14:41:53 -0400

    login

---
+++ login.php
@@ -0,0 +1,142 @@
+<?php
+// Initialize the session
+session_start();
+
+ 
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: welcome.php");
+    exit;
+}
+
+
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $servername = "aa1j2ay19mvo8dl.cew1fvzp06l6.us-east-2.rds.amazonaws.com";
+    $username = "cap";
+    $password = "capstonedb";
+ try {
+  $conn = new PDO("mysql:host=$servername;dbname=ebdb", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $name = $upassword = "";
+  $name_err = $upassword_err = $login_err = "";
+ 
+    // Check if username is empty
+    if(empty(trim($_POST["name"]))){
+        $name_err = "Please enter username.";
+    } else{
+        $name = trim($_POST["name"]);
+    }
+    
+    // Check if password is empty
+    if(empty(trim($_POST["upassword"]))){
+        $upassword_err = "Please enter your password.";
+    } else{
+        $upassword = trim($_POST["upassword"]);
+    }
+    
+    // Validate credentials
+    if(empty($name_err) && empty($upassword_err)){
+        // Prepare a select statement
+        $sql = "SELECT idusers, name, upassword FROM test WHERE name = :name";
+        
+        if($stmt = $conn->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(":name", $param_name, PDO::PARAM_STR);
+            
+            // Set parameters
+            $param_name = trim($_POST["name"]);
+            
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                // Check if username exists, if yes then verify password
+                if($stmt->rowCount() == 1){
+                    if($row = $stmt->fetch()){
+                        $id = $row["idusers"];
+                        $name = $row["name"];
+                        $hashed_password = $row["upassword"];
+                        if($upassword == $hashed_password){
+                            // Password is correct, so start a new session
+                            session_start();
+                            
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["name"] = $name;                            
+                            
+                            // Redirect user to welcome page
+                            header("location: welcome.php");
+                        } else{
+                            // Password is not valid, display a generic error message
+                            $login_err = "Invalid username or password.";
+                        }
+                    }
+                } else{
+                    // Username doesn't exist, display a generic error message
+                    $login_err = "Invalid username or password.";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            unset($stmt);
+        }
+    }
+    
+    // Close connection
+    unset($conn);
+
+
+}catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+}
+ 
+// Define variables and initialize with empty values
+
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Login</h2>
+        <p>Please fill in your credentials to login.</p>
+
+        <?php 
+        if(!empty($login_err)){
+            echo '<div class="alert alert-danger">' . $login_err . '</div>';
+        }        
+        ?>
+
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" name="name" class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $name; ?>">
+                <span class="invalid-feedback"><?php echo $name_err; ?></span>
+            </div>    
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="upassword" class="form-control <?php echo (!empty($upassword_err)) ? 'is-invalid' : ''; ?>">
+                <span class="invalid-feedback"><?php echo $upassword_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+        </form>
+    </div>
+</body>
+</html>
